d89e9b7d0db64e7a5012a2e2441142696f137712
fum1h1to <75571344+fum1h1to@users.noreply.github.com>
2023-09-17 20:30:27 +0900

    preview用に必要なファイルなどを出し分けできるか調査

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,36 @@
-const { resolve } = require('path');
+const path = require('path');
 const fs = require('fs');
 
 const beautify = require('js-beautify');
 const htmlmin = require("html-minifier");
 
+require('dotenv').config()
+
+const copyDir2root = function(srcDirName) {
+	try {
+    const files = fs.readdirSync(path.join(__dirname, srcDirName));
+
+    for (const file of files) {
+      const sourcePath = path.join(__dirname, srcDirName, file);
+      const destPath = path.join(__dirname, file);
+
+      fs.copyFileSync(sourcePath, destPath);
+      console.log(`Copied ${file} to project root`);
+    }
+
+    console.log('All files copied successfully');
+  } catch (error) {
+    console.error('Error copying files:', error);
+  }
+}
+
 module.exports = function (eleventyConfig) {
+	const productMode = process.env.PRODUCT_MODE;
+	console.log(productMode);
+	if (productMode === "preview") {
+		copyDir2root('cloudflare-config');
+	}
+
   eleventyConfig.addWatchTarget('src/')
 	eleventyConfig.addPassthroughCopy('src/assets/')
 
