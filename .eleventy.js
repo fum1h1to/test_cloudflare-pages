ce58b4ad0c2f92b3657c4c5e7ca8d8f6829c233c
fum1h1to <75571344+fum1h1to@users.noreply.github.com>
2024-12-13 23:11:41 +0900

    add: blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,72 +1,85 @@
-const path = require('path');
+const { resolve } = require('path');
 const fs = require('fs');
 
 const beautify = require('js-beautify');
 const htmlmin = require("html-minifier");
+const crypto = require('crypto');
+const { DateTime } = require('luxon');
 
-require('dotenv').config()
-
-const copyDir2root = function(srcDirName) {
-	try {
-    const sourceDir = path.join(__dirname, srcDirName);
-    const destDir = __dirname;
-
-    const items = fs.readdirSync(sourceDir);
-
-    for (const item of items) {
-      const sourcePath = path.join(sourceDir, item);
-      const destPath = path.join(destDir, item);
-
-      if (fs.statSync(sourcePath).isDirectory()) {
-        fs.renameSync(sourcePath, destPath);
-        console.log(`Moved directory ${item} to project root`);
-      }
-    }
-
-    console.log('All directories moved successfully');
-  } catch (error) {
-    console.error('Error moving directories:', error);
-  }
+function createHash(text) {
+	const uint8 = new TextEncoder().encode(text)
+	return crypto.createHash('md5').update(uint8).digest('hex');
 }
 
 module.exports = function (eleventyConfig) {
-	const productMode = process.env.PRODUCT_MODE;
-	console.log(productMode);
-	if (productMode === "preview") {
-		copyDir2root('cloudflare-config');
-	}
-
-	eleventyConfig.addGlobalData('env', process.env);
-  eleventyConfig.addWatchTarget('src/')
+	eleventyConfig.addWatchTarget('src/')
 	eleventyConfig.addPassthroughCopy('src/assets/')
 
 	eleventyConfig.on('eleventy.before', async ({ dir, runMode, outputMode }) => {
 		const outputPath = dir.output;
 
-		if(fs.existsSync(outputPath)) {
-      fs.rmSync(outputPath, { recursive: true, force: true });
+		if (fs.existsSync(outputPath)) {
+			fs.rmSync(outputPath, { recursive: true, force: true });
 			console.log(`Deleted ${outputPath}`);
-    }
+		}
 
-  });
+	});
 
-	eleventyConfig.addTransform("htmlmin", function(content) {
-    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
+	eleventyConfig.addFilter("dateToFormat", function (date, format) {
+		return DateTime.fromISO(date).toFormat(format);
+	});
 
-    return content;
-  });
-	
-	eleventyConfig.addTransform("beautify", function(content) {
-		if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+	eleventyConfig.addFilter("dateToISO", function (date) {
+		return DateTime.fromISO(date).toISO();
+	});
+
+	eleventyConfig.addFilter("slug", function (str) {
+		return createHash(str);
+	});
+
+	eleventyConfig.addCollection("posts", function (collectionApi) {
+		const posts = collectionApi.getAll()[0].data.posts;
+    
+		posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+		return posts.map((post, index) => ({
+			...post,
+			url: post.url == null ? `/posts/${eleventyConfig.getFilter("slug")(post.title + post.date)}/` : post.url,
+			slug: eleventyConfig.getFilter("slug")(post.title + post.date),
+			prevPost: posts[index - 1] || null,
+			nextPost: posts[index + 1] || null
+		}));
+	});
+
+	eleventyConfig.addCollection("recentPosts", function (collectionApi) {
+		const posts = collectionApi.getAll()[0].data.posts;
+    
+		posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+		return posts.slice(0, 5).map((post) => ({
+			...post,
+			url: post.url == null ? `/posts/${eleventyConfig.getFilter("slug")(post.title + post.date)}/` : post.url,
+			slug: eleventyConfig.getFilter("slug")(post.title + post.date),
+		}));
+	});
+
+	eleventyConfig.addTransform("htmlmin", function (content) {
+		if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+
+		return content;
+	});
+
+	eleventyConfig.addTransform("beautify", function (content) {
+		if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
 			let formated = beautify.html(content, {
 				indent_size: 2,
-  			wrap_line_length: 0,
+				wrap_line_length: 0,
 				wrapAttributes: "auto",
 				unformattedContentDelimiter: ["pre", "code"],
 				unformatted: null,
